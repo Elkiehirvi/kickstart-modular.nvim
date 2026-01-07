1b68e0655305d014dc13eec52090a18c73e56fba
elkiehirvi <lacci91@gmail.com>
2026-01-07 22:05:30 +0200

    muu

--- lua/options.lua
+++ lua/options.lua
@@ -4,7 +4,7 @@
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.o.number = false
+vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
@@ -70,5 +70,7 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.expandtab = true
 
 -- vim: ts=2 sts=2 sw=2 et
