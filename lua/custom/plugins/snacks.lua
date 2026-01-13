d439ee88ab8ba508ff0390b29489fbe623c55e76
elkiehirvi <lacci91@gmail.com>
2026-01-13 12:05:44 +0200

    Changed from leap to flash.nvim

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,9 +4,12 @@ return {
     ---@type snacks.Config
     opts = {
       image = {
-        -- your image configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        resolve = function(path, src)
+          local api = require 'obsidian.api'
+          if api.path_is_note(path) then
+            return api.resolve_attachment_path(src)
+          end
+        end,
       },
     },
   },
