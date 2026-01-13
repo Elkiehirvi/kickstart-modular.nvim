23491cc62faf15a431a270f59cd019286c30ace4
elkiehirvi <lacci91@gmail.com>
2026-01-13 21:18:12 +0200

    edited snacks.lua

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,15 +1,48 @@
 return {
-  {
-    'folke/snacks.nvim',
-    ---@type snacks.Config
-    opts = {
-      image = {
-        resolve = function(path, src)
-          local api = require 'obsidian.api'
-          if api.path_is_note(path) then
-            return api.resolve_attachment_path(src)
-          end
-        end,
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    picker = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+    image = {
+      enabled = true,
+      doc = {
+        enabled = true,
+        inline = true,
+        -- render the image in a floating window
+        -- only used if `opts.inline` is disabled
+        float = true,
+        -- Sets the size of the image
+        -- max_width = 60,
+        -- max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
+        -- max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
+        max_width = 80,
+        max_height = 40,
+        -- max_height = 30,
+        -- Apparently, all the images that you preview in neovim are converted
+        -- to .png and they're cached, original image remains the same, but
+        -- the preview you see is a png converted version of that image
+        --
+        -- Where are the cached images stored?
+        -- This path is found in the docs
+        -- :lua print(vim.fn.stdpath("cache") .. "/snacks/image")
+        -- For me returns `~/.cache/neobean/snacks/image`
+        -- Go 1 dir above and check `sudo du -sh ./* | sort -hr | head -n 5`
       },
     },
   },
