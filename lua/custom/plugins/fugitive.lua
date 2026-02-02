eb1e2e6cd6c561b7abf8ece88dc002d64d49b1d7
elkiehirvi <lacci91@gmail.com>
2026-02-02 11:31:21 +0200

    Trying to get guess-indentto work

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
     'tpope/vim-fugitive',
     config = function()
       -- Optional configuration can go here
-      vim.api.nvim_set_keymap('n', '<leader>gs', ':Gstatus<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>gs', ':G<CR>', { noremap = true, silent = true })
     end,
   },
 }
